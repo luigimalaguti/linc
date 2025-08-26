405cb1136015ae9dc4bc398e708030e6599983b8
Luigi Malaguti <malaguti.luigi@gmail.com>
2025-08-26 21:10:51 +0000

    test: header and footer

--- test/test_linc.c
+++ test/test_linc.c
@@ -3,22 +3,23 @@
 #include <stdio.h>
 
 void test_header(void) {
-    printf("%s\n", "==================================================");
-    printf("%s\n", "                    LINC TESTS                    ");
-    printf("%s\n", "--------------------------------------------------");
-    printf("%s\n", "Simple test, as a main function, to check if LINC ");
-    printf("%s\n", "works fine.");
-    printf("%s\n", "No tests are performed here, just a simple call to");
-    printf("%s\n", "a function defined in the LINC library.");
-    printf("%s\n", "==================================================");
+    printf("\n");
+    printf("%s\n", "================================================================================");
+    printf("%s\n", "                                   LINC TESTS                                   ");
+    printf("%s\n", "--------------------------------------------------------------------------------");
+    printf("%s\n", "Simple test, as a main function, to check if LINC works fine.");
+    printf("%s\n", "No tests are performed here, just a simple call to a function defined in the");
+    printf("%s\n", "LINC library.");
+    printf("%s\n", "--------------------------------------------------------------------------------");
     printf("\n");
 }
 
 void test_footer(void) {
     printf("\n");
-    printf("%s\n", "==================================================");
-    printf("%s\n", "                 LINC TESTS DONE                  ");
-    printf("%s\n", "==================================================");
+    printf("%s\n", "--------------------------------------------------------------------------------");
+    printf("%s\n", "                                LINC TESTS DONE                                 ");
+    printf("%s\n", "================================================================================");
+    printf("\n");
 }
 
 int main(void) {
@@ -28,18 +29,18 @@ int main(void) {
     // WHY -> Because the default log level is INFO
     DEBUG("%s", "Log from DEBUG");
     DEBUG_M("main", "%s", "Log from DEBUG_M");
-    linc_log("main", LINC_LEVEL_DEBUG, "test/test_linc.c", 31, "main", "%s", "Log from linc_log");
+    linc_log("main", LINC_LEVEL_DEBUG, "test/test_linc.c", 32, "main", "%s", "Log from linc_log");
 
     // These logs should not appear in the output
     // WHY -> Because the default module is "main"
     INFO_M("not_existing", "%s", "Log from INFO_M");
-    linc_log("not_existing", LINC_LEVEL_INFO, "test/test_linc.c", 36, "main", "%s", "Log from linc_log");
+    linc_log("not_existing", LINC_LEVEL_INFO, "test/test_linc.c", 37, "main", "%s", "Log from linc_log");
 
     // These logs should appear in the output
     // WHY -> Because the default log level is INFO and default module is "main"
     INFO("%s", "Log from INFO");
     INFO_M("main", "%s", "Log from INFO_M");
-    linc_log("main", LINC_LEVEL_INFO, "test/test_linc.c", 42, "main", "%s", "Log from linc_log");
+    linc_log("main", LINC_LEVEL_INFO, "test/test_linc.c", 43, "main", "%s", "Log from linc_log");
 
     test_footer();
     return 0;
